20cb157544643937401f5350088929ca21420a64
salirezahashemi <hashemi.seyedalireza313@gmail.com>
2024-07-02 20:07:46 +0330

    class decode

--- Encode_TuringMachine_And_States.py
+++ Encode_TuringMachine_And_States.py
@@ -9,60 +9,73 @@ final_state = inputM.final_state
 actions = inputM.actions
 
 
-def encode_transition(states1, start_state1, final_state1, actions1):
-    encode_array_states = []
-    tuple_states = []
-    x = 1
-    len_states = len(states1)
-    for i in range(len_states):
-        for j in range(x):
-            encode_array_states.append(1)
-        tuple_states.append({states1[i]: x})
+class Decode:
+    def __init__(self):
+        self.array_states = []
+        self.array_action = []
+        self.states1 = states
+        self.actions1 = actions
+        self.start_state1 = start_state
+        self.final_state1 = final_state
+        self._encod()
 
-        x += 1
-        encode_array_states.append(0)
-    encode_array_states.pop(len(encode_array_states) - 1)
+    def _encod(self):
+        # self.encode_array_states = encode_transition(states, start_state, final_state, actions)
+        # self.array_action = encode_transition(states, start_state, final_state, actions)
 
-    array_action = []
+        encode_array_states = []
+        tuple_states = []
+        x = 1
+        len_states = len(self.states1)
+        for i in range(len_states):
+            for j in range(x):
+                encode_array_states.append(1)
+            tuple_states.append({self.states1[i]: x})
 
-    for action in actions1:
-        for tple_state in tuple_states:
-            if tple_state.get(action[0]) is not None:
-                for i in range(tple_state.get(action[0])):
-                    array_action.append(1)
+            x += 1
+            encode_array_states.append(0)
+        encode_array_states.pop(len(encode_array_states) - 1)
+        self.array_states = encode_array_states
+
+        array_action = []
+
+        for action in self.actions1:
+            for tple_state in tuple_states:
+                if tple_state.get(action[0]) is not None:
+                    for i in range(tple_state.get(action[0])):
+                        array_action.append(1)
+                    array_action.append(0)
+
+            if action[1] != 'blank':
+                array_action.append(action[1])
+                array_action.append(0)
+            else:
+                array_action.append("#")
                 array_action.append(0)
 
-        if action[1] != 'blank':
-            array_action.append(action[1])
-            array_action.append(0)
-        else:
-            array_action.append("#")
-            array_action.append(0)
-
-        if action[2] != 'blank':
-            array_action.append(action[2])
-            array_action.append(0)
-        else:
-            array_action.append("#")
-            array_action.append(0)
-
-        if action[3] == 'l' or action[3] == 'L':
-            array_action.append(1)
-            array_action.append(0)
-        elif action[3] == 'r' or action[3] == 'R':
-            array_action.append(1)
-            array_action.append(1)
-            array_action.append(0)
-
-        for tple_state in tuple_states:
-            if tple_state.get(action[4]) is not None:
-                for i in range(tple_state.get(action[4])):
-                    array_action.append(1)
+            if action[2] != 'blank':
+                array_action.append(action[2])
+                array_action.append(0)
+            else:
+                array_action.append("#")
                 array_action.append(0)
 
-        array_action.append('$')
+            if action[3] == 'l' or action[3] == 'L':
+                array_action.append(1)
+                array_action.append(0)
+            elif action[3] == 'r' or action[3] == 'R':
+                array_action.append(1)
+                array_action.append(1)
+                array_action.append(0)
 
-    array_action.pop(len(array_action) - 1)
+            for tple_state in tuple_states:
+                if tple_state.get(action[4]) is not None:
+                    for i in range(tple_state.get(action[4])):
+                        array_action.append(1)
+                    array_action.append(0)
 
-    yield encode_array_states
-    yield array_action
+            array_action.append('$')
+
+        array_action.pop(len(array_action) - 1)
+
+        self.array_action = array_action
---
+++ test.py
@@ -0,0 +1,7 @@
+from Encode_TuringMachine_And_States import Encode
+
+encode_array_states = Encode()
+array_action = Encode()
+
+print(encode_array_states)
+print(array_action)
